5b512f79d4086b2a2d5151cd38f42f3e1a1c0d8b
caio rodrigues <caiorodriguesmll@gmail.com>
2024-05-13 22:31:08 -0300

    new

---
+++ js/questions.js
@@ -0,0 +1,48 @@
+// criar array passando numero, questão, opção e respostas
+let questions = [
+    {
+        numb: 1,
+        question:"Qual é o principal objetivo do gerenciamento de coleta de lixo?",
+        answer: "d) Todas as opções acima",
+        options: [
+            "a) Reduzir a quantidade de lixo produzido",
+            "b) Garantir que todo o lixo seja reciclado",
+            "c) Minimizar o impacto ambiental do lixo",
+            "d) Todas as opções acima"
+        ]
+    },
+    {
+        numb: 2,
+        question:"Qual dos seguintes não é um método de disposição de resíduos sólidos? ",
+        answer: "d) Evaporação",
+        options: [
+            "a) Aterro sanitário",
+            "b) Incineração",
+            "c) Reciclagem",
+            "d) Evaporação"
+        ]
+    },
+    {
+        numb: 3,
+        question:"O que é compostagem no contexto do gerenciamento de resíduos?",
+        answer: "b) Um processo de transformação de resíduos orgânicos em nutrientes para o solo",
+        options: [
+            "a) Um processo de queima de resíduos",
+            "b) Um processo de transformação de resíduos orgânicos em nutrientes para o solo",
+            "c) Um processo de separação de resíduos",
+            "d) Um processo de armazenamento de resíduos"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Qual é a importância da coleta seletiva de lixo?",
+        answer: "d) Todas as opções acima",
+        options: [
+            "a) Reduz a quantidade de lixo que vai para os aterros sanitários",
+            "b) Aumenta a eficiência da reciclagem",
+            "c) Reduz a poluição causada pela disposição inadequada de resíduos",
+            "d) Todas as opções acima"
+        ]
+    },
+    
+]
